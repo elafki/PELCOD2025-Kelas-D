1e3882a899a3bcaa0c15cda648e291865c87cd12
elafki <asmirafiqi@gmail.com>
2025-09-14 00:05:50 +0700

    Create tugassganjil atau tidak.py

---
+++ 100_090asmirafiki/tugassganjil atau tidak.py
@@ -0,0 +1,14 @@
+angka_genap = 4 # int dari angka genap
+angka_ganjil = 5 # int dari angka ganjil
+tentukan = 'tentukan genap ganjilnya  '
+
+
+# Program untuk menentukan apakah angka genap atau ganjil
+print('tentukan genap ganjilnya...' + tentukan)
+print(angka_genap)
+if angka_genap % 2 == 0:
+    print(str(angka_genap) + 'adalah angka genap')
+print(angka_ganjil)
+if angka_ganjil % 2 != 0:
+    print(str(angka_ganjil)+ 'adalah angka ganjil')
+
